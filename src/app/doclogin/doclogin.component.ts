7fb63392e1930348a6a40e03df31fc19c3bac562
D-Kamunya <dennismuriithik@gmail.com>
2020-11-17 23:50:37 +0300

    Validate doctor login form

--- src/app/doclogin/doclogin.component.ts
+++ src/app/doclogin/doclogin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthServiceService} from '../core/authentication/auth-service.service'
 @Component({
   selector: 'app-doclogin',
   templateUrl: './doclogin.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocloginComponent implements OnInit {
 
-  constructor() { }
+	public loginuser:any
+	
+  constructor(public _authService:AuthServiceService) { }
 
   ngOnInit(): void {
+
+  	this.loginuser={
+  		email:'',
+  		password:''
+  	}
+  }
+
+  docLogin=()=>{
+  	// this._authService.loginUser(this.loginuser)
+  	console.log(this.loginuser)
   }
 
 }
