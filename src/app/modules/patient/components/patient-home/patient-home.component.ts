b1ca50b27780479debaee5ade8f27554de689d9b
priscillapepe <priscillaungai99@gmail.com>
2020-11-18 22:55:33 +0300

    Create records service and display count on patient homepage

--- src/app/modules/patient/components/patient-home/patient-home.component.ts
+++ src/app/modules/patient/components/patient-home/patient-home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AppointmentService} from '../../../../core/http/appointment-service/appointment.service'
 import {BillService} from '../../../../core/http/bill-service/bill.service'
 import {DoctorService} from '../../../../core/http/doctor-service/doctor.service'
+import {RecordsService} from '../../../../core/http/records-service/records.service'
 
 @Component({
   selector: 'app-patient-home',
@@ -10,12 +11,16 @@ import {DoctorService} from '../../../../core/http/doctor-service/doctor.service
 })
 export class PatientHomeComponent implements OnInit {
 
-  constructor(public _appService: AppointmentService,public _billService: BillService,public _docService:DoctorService) { }
+	
+	user=JSON.parse(localStorage.getItem('auth_user'))
+
+  constructor(public _appService: AppointmentService,public _billService: BillService,public _docService:DoctorService,public _recService:RecordsService) { }
 
   ngOnInit(): void {
   	this._appService.getPatientApps()
   	this._billService.getPatientBills()
   	this._docService.getAllDoctors()
+  	this._recService.getPatientRecords()
   }
 
 }
