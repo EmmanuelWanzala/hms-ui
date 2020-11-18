1e9f4f7392f0dc2db3686c399e343922a43946b7
ibukamshindi <ibukamshindi@gmail.com>
2020-11-18 23:55:51 +0300

    Display all doctors on patient db doctors page

--- src/app/modules/patient/components/patient-doctors/patient-doctors.component.ts
+++ src/app/modules/patient/components/patient-doctors/patient-doctors.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DoctorService} from '../../../../core/http/doctor-service/doctor.service'
+
 
 @Component({
   selector: 'app-patient-doctors',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientDoctorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _docService:DoctorService) { }
 
   ngOnInit(): void {
+  	this._docService.getAllDoctors()
   }
   data=['https://randomuser.me/api/portraits/men/83.jpg','https://randomuser.me/api/portraits/men/59.jpg','https://randomuser.me/api/portraits/men/80.jpg','https://randomuser.me/api/portraits/women/16.jpg','https://randomuser.me/api/portraits/women/89.jpg']
 
