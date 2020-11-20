a37b56821e8994e4fcc77806d1c1ef5392db23d4
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-11-20 14:30:01 +0300

    Create get all patients fn and display count on docdb home page

---
+++ src/app/core/http/patient-service/patient.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+
+	// http options used for making API calls
+  private httpOptions: any;
+  allPatients:any
+  errors:boolean=false
+  constructor(private http:HttpClient) { }
+
+
+
+  public getAllPatients() {
+
+    this.http.get(`${environment.api_url}/accounts/api/patients`, this.httpOptions).subscribe(
+      data => {
+        this.allPatients=data
+        console.log(data)      
+      },
+      err => {
+        console.log(err)
+        this.errors=true
+          }
+    );
+  }
+}
--- src/app/modules/doctor/components/doctor-home/doctor-home.component.ts
+++ src/app/modules/doctor/components/doctor-home/doctor-home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AppointmentService} from '../../../../core/http/appointment-service/appointment.service'
-
+import {PatientService} from '../../../../core/http/patient-service/patient.service'
 @Component({
   selector: 'app-doctor-home',
   templateUrl: './doctor-home.component.html',
@@ -10,10 +10,11 @@ export class DoctorHomeComponent implements OnInit {
 
 	user=JSON.parse(localStorage.getItem('auth_user'))
 
-  constructor(public _appService: AppointmentService) { }
+  constructor(public _appService: AppointmentService,public _patService:PatientService) { }
 
   ngOnInit(): void {
   	this._appService.getDoctorApps()
+  	this._patService.getAllPatients()
   }
 
 }
