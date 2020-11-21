82861e06680aae645b38f89e246830ea5f3db412
priscillapepe <priscillaungai99@gmail.com>
2020-11-21 12:02:46 +0300

    Validate patient diagnosis form inputs

--- src/app/modules/doctor/components/doctor-patients/doctor-patients.component.ts
+++ src/app/modules/doctor/components/doctor-patients/doctor-patients.component.ts
@@ -7,12 +7,36 @@ import {PatientService} from '../../../../core/http/patient-service/patient.serv
 })
 export class DoctorPatientsComponent implements OnInit {
 
+
+	public case:any
+
+	public user:any
+
   constructor(public _patService:PatientService) { }
 
   ngOnInit(): void {
+  	this.user=JSON.parse(localStorage.getItem('auth_user'))
+  	this.case={
+  		patient:'',
+  		doctor:this.user.user.id,
+  		case_title:'',
+  		diagnosis:'',
+  		medication:'',
+  		medication_quantity:'',
+  		service:'',
+  		admission_days:''
+  	}
   	this._patService.getAllPatients()
   }
 
+
+  makeDiagnosis=()=>{
+  	console.log(this.case)
+  }
+
+  setPatientId=(patid)=>{
+  	this.case.patient=patid
+  }
 
 
 }
