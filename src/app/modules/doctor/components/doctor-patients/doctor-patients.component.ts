913c722e32775d5ee4f812024fdd35c5dcfe4ba4
priscillapepe <priscillaungai99@gmail.com>
2020-11-21 13:49:13 +0300

    Create logic to craete patient case

--- src/app/modules/doctor/components/doctor-patients/doctor-patients.component.ts
+++ src/app/modules/doctor/components/doctor-patients/doctor-patients.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {PatientService} from '../../../../core/http/patient-service/patient.service'
 import {MedicationService} from '../../../../core/http/medication-service/medication.service'
 import {ServicesService} from '../../../../core/http/services-service/services.service'
+import {RecordsService} from '../../../../core/http/records-service/records.service'
 
 @Component({
   selector: 'app-doctor-patients',
@@ -15,9 +16,9 @@ export class DoctorPatientsComponent implements OnInit {
 
 	public user:any
 
-  constructor(public _patService:PatientService,public _medService:MedicationService,public _sevService:ServicesService) { }
+  constructor(public _patService:PatientService,public _medService:MedicationService,public _sevService:ServicesService,public _recService:RecordsService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   	this.user=JSON.parse(localStorage.getItem('auth_user'))
   	this.case={
   		patient:'',
@@ -25,7 +26,7 @@ export class DoctorPatientsComponent implements OnInit {
   		case_title:'',
   		diagnosis:'',
   		medication:'',
-  		medication_quantity:'',
+  		medication_quantity:0,
   		service:'',
   		admission_days:''
   	}
@@ -36,11 +37,18 @@ export class DoctorPatientsComponent implements OnInit {
 
 
   makeDiagnosis=()=>{
-  	console.log(this.case)
+  	this._recService.createRecord(this.case)
   }
 
   setPatientId=(patid)=>{
   	this.case.patient=patid
+  		this.case.case_title=''
+  		this.case.diagnosis=''
+  		this.case.medication=''
+  		this.case.medication_quantity=0
+  		this.case.service=''
+  		this.case.admission_days=''
+  	
   }
 
 
