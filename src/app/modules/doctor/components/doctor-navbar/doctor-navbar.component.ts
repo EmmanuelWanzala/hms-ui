b11ab39a4108c532ccd90c9d6754de05428c7d4d
D-Kamunya <dennismuriithik@gmail.com>
2020-11-25 12:08:57 +0300

    Display profile pic on navbar

--- src/app/modules/doctor/components/doctor-navbar/doctor-navbar.component.ts
+++ src/app/modules/doctor/components/doctor-navbar/doctor-navbar.component.ts
@@ -8,10 +8,11 @@ import {AuthServiceService} from "../../../../core/authentication/auth-service.s
   styleUrls: ['./doctor-navbar.component.css']
 })
 export class DoctorNavbarComponent implements OnInit {
-
+  public profile:any
   constructor(public _authService:AuthServiceService) { }
 
   ngOnInit(): void {
+  	this.profile = JSON.parse(localStorage.getItem('auth_user'))
   }
 
 }
--- src/app/modules/patient/components/patient-navbar/patient-navbar.component.ts
+++ src/app/modules/patient/components/patient-navbar/patient-navbar.component.ts
@@ -7,10 +7,11 @@ import {AuthServiceService} from "../../../../core/authentication/auth-service.s
   styleUrls: ['./patient-navbar.component.css']
 })
 export class PatientNavbarComponent implements OnInit {
-
+  public profile:any
   constructor(public _authService:AuthServiceService) { }
 
   ngOnInit(): void {
+  	this.profile = JSON.parse(localStorage.getItem('auth_user'))
   }
 
 }
